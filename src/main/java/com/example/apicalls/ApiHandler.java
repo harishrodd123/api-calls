5fae8a5f996455327be47e8f993c3859d7b02857
harish <harish_rodd@epam.com>
2022-10-25 15:22:08 +0530

    [ get user endpoint added ]

--- src/main/java/com/example/apicalls/ApiHandler.java
+++ src/main/java/com/example/apicalls/ApiHandler.java
@@ -1,16 +1,13 @@
 package com.example.apicalls;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 
 @RestController
 public class ApiHandler {
 
     @GetMapping(value = "/users")
     public String get() {
-
         RestTemplate template = new RestTemplate();
         return template.getForObject("https://jsonplaceholder.typicode.com/todos", String.class);
 
@@ -18,7 +15,6 @@ public class ApiHandler {
 
     @GetMapping(value = "/user")
     public String getUser() {
-
         RestTemplate template = new RestTemplate();
         return template.getForObject("https://jsonplaceholder.typicode.com/todos/1", String.class);
     }
